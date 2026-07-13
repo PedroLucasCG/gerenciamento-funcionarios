a5044735df0528d0977f266c0f2047f0adc06f84
PedroLucasCG <pedrowwww44@gmail.com>
2026-07-13 04:55:48 -0300

    commit inicial

---
+++ src/main/java/com/wakanda/gestao_funcionarios/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.wakanda.gestao_funcionarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
